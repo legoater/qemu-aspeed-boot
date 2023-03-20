fb1f0933c3d885025565298e5cd548354ab16cce
Cédric Le Goater <clg@kaod.org>
2023-03-20 15:59:27 +0100

    Add github worflow
    Signed-off-by: Cédric Le Goater <clg@kaod.org>

---
+++ qemu-ci/build-ubuntu-22.04.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -uo pipefail
+set -e
+set -vx
+
+git clone --depth=1 -b aspeed-8.0 https://github.com/legoater/qemu.git
+cd qemu
+export CC="ccache gcc"
+export CXX="ccache g++"
+./configure --target-list=arm-softmmu --prefix=$(pwd)/install
+make -j $(grep -c processor /proc/cpuinfo) install
+cd ..
+
+./aspeed-boot.sh -q --prefix=qemu/install/
