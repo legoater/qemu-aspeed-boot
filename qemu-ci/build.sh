39286a13f39f16c43d82e98ed4b5d7fc3fc9d3e9
Cédric Le Goater <clg@redhat.com>
2024-06-06 08:52:09 +0200

    Add aarch64 build
    Signed-off-by: Cédric Le Goater <clg@redhat.com>

--- qemu-ci/build.sh
+++ qemu-ci/build.sh
@@ -8,7 +8,7 @@ git clone --depth=1 -b aspeed-9.1 https://github.com/legoater/qemu.git
 cd qemu
 export CC="ccache gcc"
 export CXX="ccache g++"
-./configure --target-list=arm-softmmu --prefix=$(pwd)/install
+./configure --target-list=arm-softmmu,aarch64-softmmu --prefix=$(pwd)/install
 make -j $(grep -c processor /proc/cpuinfo) install
 cd ..
 
